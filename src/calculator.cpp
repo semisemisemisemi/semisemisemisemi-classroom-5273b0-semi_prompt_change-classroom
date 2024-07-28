39d1d1611f25ee1ef8eaa563d50350fd85586656
leedyun <105830643+leedyun@users.noreply.github.com>
2024-07-29 00:58:29 +0900

    Update calculator.cpp

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -6,11 +6,11 @@ int Calculator::add(int a, int b) {
 }
 
 int Calculator::subtract(int a, int b) {
-    return a - b;
+    return b - a;
 }
 
 int Calculator::multiply(int a, int b) {
-    return a * b;
+    return a + b;
 }
 
 double Calculator::divide(int a, int b) {
